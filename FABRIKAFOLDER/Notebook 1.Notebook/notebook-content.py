dcb693ab9c6194e4ce375a26e5d24b28223306b2
alokin-cakic <nikolac@gmail.com>
2025-10-18 19:56:24 +0200

    Committing 1 item from workspace 64b38f80-11d9-4e4c-82d0-a196fb68c9d7

---
+++ FABRIKAFOLDER/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "2056c230-d916-4e6f-9d7b-9725a2b55ed0",
+# META       "default_lakehouse_name": "TutorialLakehouse",
+# META       "default_lakehouse_workspace_id": "64b38f80-11d9-4e4c-82d0-a196fb68c9d7",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "2056c230-d916-4e6f-9d7b-9725a2b55ed0"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+# probni notebook za github
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
